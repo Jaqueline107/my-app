e93cc65a237931df96d626aba4955ba12a5f8187
jaqueline107 <0000107344630xsp@al.educacao.sp.gov.br>
2024-01-02 13:14:38 -0300

    Create app with Expo.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
+
+import { Routes } from './src/routes';
+import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_700Bold,
+    Inter_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <StatusBar style="dark" translucent backgroundColor="transparent" />
+        <Routes />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
